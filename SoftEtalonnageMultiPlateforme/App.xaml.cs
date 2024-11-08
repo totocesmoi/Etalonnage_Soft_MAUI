99dde7902d079524a99dce20ea368c5ddf670538
Ludivine BORGES <t.muzard63@gmail.com>
2024-11-08 14:58:48 +0100

    Project creation

---
+++ SoftEtalonnageMultiPlateforme/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftEtalonnageMultiPlateforme
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
