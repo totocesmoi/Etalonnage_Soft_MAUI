52ad14510abe65bac061d3dc68b9490e5cfb48ef
Thomas MUZARD <t.muzard63@gmail.com>
2024-11-21 15:27:08 +0100

    ConverterRole OK

---
+++ SoftEtalonnageMultiPlateforme/Converters/RoleToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftEtalonnageMultiPlateforme.Converters
+{
+    public class RoleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Role role)
+            {
+                return role.ToString();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string roleString && Enum.TryParse(typeof(Role), roleString, out var role))
+            {
+                return role;
+            }
+            return Role.Operator; // Default value
+        }
+    }
+}
